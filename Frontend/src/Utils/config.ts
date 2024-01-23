737a3f6e350532e1bd540275dd4b143e88aa70be
Baruch Kaminer <baruchkaminer@gmail.com>
2024-01-23 18:57:38 +0200

    build

--- Frontend/src/Utils/config.ts
+++ Frontend/src/Utils/config.ts
@@ -1,7 +1,7 @@
 class AppConfig{
 
     // public serverUrl = "http://localhost:3001/api/";
-    public serverUrl = "http://localhost:5000/api/";
+    public serverUrl = "http://localhost:13553/api/";
     // public serverUrl = `${process.env.PORT}/api/`;
     // public serverUrl = `https://my-vacations-2856224fbe83.herokuapp.com/api/`;
     public registerUrl = this.serverUrl + 'auth/register/';
